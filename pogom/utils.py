c62874f5834262806f6cc096a55066eedee452db
Harvey Tang <harvey.xw.tang@gmail.com>
2016-07-28 19:19:05 +0000

    Allow external access

--- pogom/utils.py
+++ pogom/utils.py
@@ -26,7 +26,7 @@ def get_args():
     parser.add_argument('-l', '--location', type=parse_unicode, help='Location, address or coordinates', required=True)
     parser.add_argument('-r', '--radius', help='Search radius [m]', required=True, type=int)
 
-    parser.add_argument('-H', '--host', help='Set web server listening host', default='127.0.0.1')
+    parser.add_argument('-H', '--host', help='Set web server listening host', default='0.0.0.0')
     parser.add_argument('-P', '--port', type=int, help='Set web server listening port', default=5000)
 
     parser.add_argument('-d', '--debug', type=str.lower, help='Debug Level [info|debug]', default=None)
